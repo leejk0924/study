5534b25b08627fdd0356c68e6b3db270dcbf9a06
Jung-Kwon Lee <akdirdl@gmail.com>
2022-06-28 00:53:49 +0900

    lecture: 연관관계 매핑 기초(단방향 연관관계)

--- spring/jpa/ex-hello-jpa/src/main/java/hellojpa/Member.java
+++ spring/jpa/ex-hello-jpa/src/main/java/hellojpa/Member.java
@@ -3,10 +3,7 @@ package hellojpa;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 // @Entity 애너테이션은 JPA 를 사용하는 객체라고 인식시켜주고 관리하게 한다.
 @Entity
@@ -15,11 +12,16 @@ import javax.persistence.Table;
 @Getter
 public class Member {
 
-    @Id     // PK 설정하는 애너테이션이고 반드시 설정해야한다.
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
     
-    @Column(name="name")    // 필드명 매핑
-    private String name;
+    @Column(name="USERNAME")    // 필드명 매핑
+    private String userName;
 
-
+    //    @Column(name = "TEAM_ID")
+//    private Long teamId;
+    @ManyToOne
+    @JoinColumn(name="TEAM_ID")     // DB와의 관계와 join 하는 컬럼명을 적어주어야 한다.
+    private Team team;
 }
