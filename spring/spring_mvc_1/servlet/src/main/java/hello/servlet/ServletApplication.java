8c92ba3876cf6f0d506be656e8e3c932bb42d920
Jung-Kwon Lee <akdirdl@gmail.com>
2022-06-28 22:21:18 +0900

    lecture: 서블릿(Hello 서블릿)

--- spring/spring_mvc_1/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ spring/spring_mvc_1/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,12 +2,14 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan   // 스프링이 해당 클래스의 패키지포함, 하위 패키지를 전부 검색하여 서블릿을 자동으로 등록
 @SpringBootApplication
 public class ServletApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServletApplication.class, args);
     }
-    
+
 }
