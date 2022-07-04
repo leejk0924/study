8e4cffb5840a2a3f027532559a6ae7f57709a112
Jung-Kwon Lee <akdirdl@gmail.com>
2022-07-04 23:30:38 +0900

    lecture: 스프링 MVC - 기본 기능 (로깅 간단히 알아보기)

---
+++ spring/spring_mvc_1/springmvc/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,32 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+    // 현재 나의 클래스를 인자로 받음 (LogTestController.class 도 가능)
+    // @Slf4j 사용하면 자동으로 됨
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+        
+        // 로그 레벨 (개발 서버는 debug 출력, 운영 서버는 info 출력)
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+}
