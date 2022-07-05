cf45108b0dc15b9a75480949f37d0cb20ad0858e
Jung-Kwon Lee <akdirdl@gmail.com>
2022-07-06 00:06:39 +0900

    lecture: 스프링 MVC - 기본 기능 (HTTP 요청 메시지 - 단순 텍스트)

---
+++ spring/spring_mvc_1/springmvc/springmvc/src/main/java/hello/springmvc/basic/requestmapping/request/RequestBodyStringController.java
@@ -0,0 +1,65 @@
+package hello.springmvc.basic.requestmapping.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@Controller
+public class RequestBodyStringController {
+    
+    // HTTP 메세지 컨버터
+    
+    @PostMapping("/request-body-string-v1")
+    public void requestBodyString(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        // 바이트코드로 값을 받을 땐 어떤 인코딩으로 받을지 지정
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}", messageBody);
+        response.getWriter().write("ok");
+    }
+
+    @PostMapping("/request-body-string-v2")
+    public void requestBodyStringV2(InputStream inputStream, Writer responseWriter) throws IOException {
+
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}", messageBody);
+        responseWriter.write("ok");
+    }
+
+    @PostMapping("/request-body-string-v3")
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) {
+        // HttpEntity : HTTP header 와 body 정보를 편리하게 조회한다.
+        // 요청 파라미터를 조회하는 기능과 관계없음
+        String messageBody = httpEntity.getBody();
+
+        log.info("messageBody={}", messageBody);
+        // HttpEntity 는 view 조회를 하지 않은다.
+        return new HttpEntity<>("ok");
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-string-v4")
+    public String requestBodyStringV4(@RequestBody String messageBody) {
+
+        log.info("messageBody={}", messageBody);
+        return "ok";
+    }
+}
