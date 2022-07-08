7b06afb52b29a6da55018d0d64c9d4609784820b
Jung-Kwon Lee <akdirdl@gmail.com>
2022-07-08 14:39:52 +0900

    lecture: 스프링 MVC - 스프링 MVC - 웹페이지 만들기(PRG Post/Redirect/Get)

--- spring/spring_mvc_1/project/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ spring/spring_mvc_1/project/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -69,12 +69,17 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(Item item) {
         // @ModelAttribute 는 생략 가능
-
         itemRepository.save(item);
         return "basic/item";
+    }
+    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        // @ModelAttribute 는 생략 가능
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
     }
 
     @GetMapping("/{itemId}/edit")
