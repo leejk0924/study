4553bb0369fc38470dddbd13607b4be8b01c05fb
Jung-Kwon Lee <akdirdl@gmail.com>
2022-07-30 09:26:35 +0900

    lecture : 타임리프 - 기본기능(기본 객체들)

--- spring/spring_mvc_2/thymeleaf_basic/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ spring/spring_mvc_2/thymeleaf_basic/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -2,11 +2,13 @@ package hello.thymeleaf.basic;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -43,6 +45,7 @@ public class BasicController {
         model.addAttribute("userMap", map);
         return "basic/variable";
     }
+
     @AllArgsConstructor
     @Data
     static class User {
@@ -50,4 +53,16 @@ public class BasicController {
         private int age;
     }
 
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "/basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
+    }
 }
