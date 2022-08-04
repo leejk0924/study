e6082ceb2e5eb9ff425a5ff5d1f8a5e39d957f92
Jung-Kwon Lee <akdirdl@gmail.com>
2022-08-04 11:45:37 +0900

    lecture : 메시지, 국제화 (스프링 메시지 소스 설정)

--- spring/spring_mvc_2/message/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ spring/spring_mvc_2/message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,6 +2,9 @@ package hello.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -9,5 +12,14 @@ public class ItemServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
+
+	// 스프링 부트에서 message 설정을 아래와같이 자동으로 설정해 준다.
+//	@Bean
+//	public MessageSource messageSource() {
+//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//		messageSource.setBasenames("messages", "errors");		// 여러 파일을 한번에 지정할 수 있다.
+//		messageSource.setDefaultEncoding("utf-8");
+//		return messageSource;
+//	}
 
 }
