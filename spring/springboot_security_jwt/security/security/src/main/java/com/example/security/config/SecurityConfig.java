6ca105ea1538a112e38d59af327b1ac505a95306
Jung-Kwon Lee <akdirdl@gmail.com>
2022-08-17 14:28:55 +0900

    lecture : 스프링 시큐리티 기본 (스프링부트 시큐리티 2강 - 시큐리티 설정)

---
+++ spring/springboot_security_jwt/security/security/src/main/java/com/example/security/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity  // 활성화 : 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated()    // /user/** 일 경우 인증 요청
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")  // ROLE_ADMIN 이거나 ROLE_MANAGER 일 경우에만 접속
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")       // admin 권한일 경우만 접속 가능
+                .anyRequest().permitAll()      // 다른 요청은 다 허용
+                // 권한이 없지만 권한있는 페이지에 접속할 경우 login 페이지로 이동
+                .and()
+                .formLogin()
+                .loginPage("/login");
+    }
+}
--- spring/springboot_security_jwt/security/security/src/main/java/com/example/security/controller/IndexController.java
+++ spring/springboot_security_jwt/security/security/src/main/java/com/example/security/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.example.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -13,4 +14,35 @@ public class IndexController {
         // 뷰리졸버 설정 : templates (prefix), .mustache(suffix) 생략가능 (의존성 주입되었다면)
         return "index"; // src/main/resources/templates/index.mustache
     }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    // 스프링 시큐리티 해당주소를 가져간다.  - securityConfig 파일 생성 후 작동 안함
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료됨!";
+    }
 }
+
