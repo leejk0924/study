c7d3ff95b31bb725ffee9d30075b3ea153bbb07d
Jung-Kwon Lee <akdirdl@gmail.com>
2022-08-17 15:52:40 +0900

    lecture : 스프링 시큐리티 기본(스프링부트 시큐리티 3강-시큐리티 회원가입)

--- spring/springboot_security_jwt/security/security/src/main/java/com/example/security/config/SecurityConfig.java
+++ spring/springboot_security_jwt/security/security/src/main/java/com/example/security/config/SecurityConfig.java
@@ -1,13 +1,22 @@
 package com.example.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity  // 활성화 : 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해 준다.
+    @Bean
+    public BCryptPasswordEncoder encodePwd(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -19,6 +28,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 권한이 없지만 권한있는 페이지에 접속할 경우 login 페이지로 이동
                 .and()
                 .formLogin()
-                .loginPage("/login");
+                .loginPage("/loginForm");
     }
 }
