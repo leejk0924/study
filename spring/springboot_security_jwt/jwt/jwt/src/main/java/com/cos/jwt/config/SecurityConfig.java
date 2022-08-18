9d4146dba08b8f96938b2d909ef9f9d9a236393f
Jung-Kwon Lee <akdirdl@gmail.com>
2022-08-19 01:36:58 +0900

    lecture: 스프링 시큐리티 JWT 서버구축(JWT Bearer 인증방식)

--- spring/springboot_security_jwt/jwt/jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ spring/springboot_security_jwt/jwt/jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -27,6 +27,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilter(corsFilter)      // @CrossOrigin (인증이 없을 경우), 시큐리티 필터에 등록(인증이 있을 경우)
                 // http 로그인방식 사용 X
                 .formLogin().disable()
+                // HTTP header 에 Authorization 을 담아서 요청하는 방식이 httpBasic 방식 (인증 요청시 사용되는 ID, PW 가 평문으로 보내짐)
+                // bearer Authentication 방식은 ID와 PW 으로 토큰을 만들어 전송하는 방식으로 httpBasic 보다는 안전하다 [bearer 방식을 사용하려면 httpBasic()을 disable 해야한다.]
                 .httpBasic().disable()
                 .authorizeRequests()
                 .antMatchers("/api/v1/user/**")
@@ -36,7 +38,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/v1/admin/**")
                 .access("hasRole('ROLE_ADMIN')")
                 .anyRequest().permitAll();
-
     }
 }
 /**
