b8b8142689033c365696bcc89ede4763ef31ab7b
Jung-Kwon Lee <akdirdl@gmail.com>
2022-08-19 18:15:47 +0900

    lecture:스프링 시큐리티 JWT 서버구축(JWT토큰 만들어서 응답하기)

--- spring/springboot_security_jwt/jwt/jwt/src/main/java/com/cos/jwt/config/auth/jwt/JwtAuthenticationFilter.java
+++ spring/springboot_security_jwt/jwt/jwt/src/main/java/com/cos/jwt/config/auth/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.cos.jwt.config.auth.jwt;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.cos.jwt.config.auth.PrincipalDetails;
 import com.cos.jwt.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,6 +17,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 // 스프링 시큐리티에서 UsernamePasswordAuthenticationFilter 가 있음
 // login 요청해서 username, password 전송하면 (post)
@@ -73,6 +76,17 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
         System.out.println("successfulAuthentication 실행됨 : 인증이 완료되었다는 뜻");
-        super.successfulAuthentication(request, response, chain, authResult);
+        PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+
+        // HMAC512 암호 방식
+        String jwtToken = JWT.create()
+//                .withSubject(principalDetails.getUsername())
+                .withSubject("cos토큰")
+                .withExpiresAt(new Date(System.currentTimeMillis() + (60000 * 30)))    // 만료시간 설정 (30분 설정)
+                .withClaim("id", principalDetails.getUser().getId())
+                .withClaim("username", principalDetails.getUser().getUsername())
+                .sign(Algorithm.HMAC512("cos"));
+
+        response.addHeader("Authorization", "Bearer " + jwtToken);  // "Bearer " <- 한칸 띄어쓰기 해줘야함
     }
 }
