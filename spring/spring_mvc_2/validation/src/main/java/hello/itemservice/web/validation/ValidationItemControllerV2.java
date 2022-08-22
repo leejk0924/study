d6527d1e857d13065fc8cdae033516aba1844050
Jung-Kwon Lee <akdirdl@gmail.com>
2022-08-22 21:12:40 +0900

    lecture:스프링 MVC 2편 (검증1-Validation)
    검증1-Validation(FieldError, ObjectError)

--- spring/spring_mvc_2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ spring/spring_mvc_2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -44,7 +44,7 @@ public class ValidationItemControllerV2 {
     }
 
     //!순서 중요! BindingResult 는 ModelAttribute 바로 다음에 와야한다. (Item 객체에 대한 결과를 받기 위해서 바로 뒤에 와야한다.)
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         // v1에서 errors 를 BindingResult 가 대체 한다.
         if (!StringUtils.hasText(item.getItemName())) {
@@ -64,6 +64,43 @@ public class ValidationItemControllerV2 {
             if (resultPrice < 10000) {
                 // 글로벌 에러는 ObjectError 에 넣어준다.
                 bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    // 잘못된 Item 이 들어온다면 bindingResult 에 먼저 담고 해당 컨트롤러를 호출해준다.
+//    bindingResult.addError(new FieldError("item", "itemName", "잘못 입력된 데이터", true, null, null, "상품 이름은 필수 입니다.")); 와 같이 먼저 입력된다.
+    // rejectedValue 는 오류 발생 시 사용자 입력 값을 저장하는 필드이다. / bindingFailure 는 바인딩 성공여부를 적는 것이며 위 코드는 바인딩 성공했기 떄문에 true 이다.
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        // FiledError 는 오류 발생 시 사용자 입력 값을 저장하는 기능을 제공한다.
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수 입니다."));
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1,000 ~ 1,000,000 까지 허용합니다."));
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999 까지 허용합니다."));
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
             }
         }
 
