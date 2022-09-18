b4d5cbd831e642b5d2ab85985ac7e8d2299aace4
Jung-Kwon Lee <akdirdl@gmail.com>
2022-09-19 00:17:43 +0900

    lecture:스프링 MVC 2편 (로그인 처리1 = 쿠키, 세션)
    로그인 처리1 - 쿠키, 세션 (로그인 처리하기 - 쿠키 사용)

--- spring/spring_mvc_2/login/src/main/java/hello/login/web/HomeController.java
+++ spring/spring_mvc_2/login/src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,43 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+    
+//    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    // 로그인 안한 사용자도 들어와야 하므로 required = false 설정
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name="memberId",  required = false) Long memberId, Model model) {
+        System.out.println("값 : "+memberId);
+        if (memberId == null) {
+            return "home";
+        }
+        
+        // 로그인
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+
     }
 }
