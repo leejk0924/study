5982e6df23544d415336f20277d9674f4b6c4351
Jung-Kwon Lee <akdirdl@gmail.com>
2022-09-20 01:01:59 +0900

    lecture:스프링 MVC 2편 (로그인 처리1 = 쿠키, 세션)
    로그인 처리1 - 쿠키, 세션 (로그인 처리하기 - 직접만든 세션 적용)

--- spring/spring_mvc_2/login/src/main/java/hello/login/web/HomeController.java
+++ spring/spring_mvc_2/login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 @Slf4j
@@ -17,20 +19,21 @@ import java.util.Optional;
 public class HomeController {
 
     private final MemberRepository memberRepository;
-    
-//    @GetMapping("/")
+    private final SessionManager sessionManager;
+
+    //    @GetMapping("/")
     public String home() {
         return "home";
     }
 
     // 로그인 안한 사용자도 들어와야 하므로 required = false 설정
-    @GetMapping("/")
-    public String homeLogin(@CookieValue(name="memberId",  required = false) Long memberId, Model model) {
-        System.out.println("값 : "+memberId);
+//    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        System.out.println("값 : " + memberId);
         if (memberId == null) {
             return "home";
         }
-        
+
         // 로그인
         Member loginMember = memberRepository.findById(memberId);
         if (loginMember == null) {
@@ -39,5 +42,21 @@ public class HomeController {
         model.addAttribute("member", loginMember);
         return "loginHome";
 
+    }
+
+    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+
+        // 세션 관리자에 저장된 회원정보 조회
+        Member member = (Member) sessionManager.getSession(request);
+
+        // 로그인
+
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+        return "loginHome";
     }
 }
