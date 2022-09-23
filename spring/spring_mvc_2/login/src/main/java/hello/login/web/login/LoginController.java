ab2119ba4cced18bf25d0a54be65b6d7f662397b
Jung-Kwon Lee <akdirdl@gmail.com>
2022-09-23 23:59:55 +0900

    lecture:스프링 MVC 2편 (로그인 처리2 - 필터, 인터셉터)
    필터, 인터셉터 (서블릿 필터 - 인증 체크)

--- spring/spring_mvc_2/login/src/main/java/hello/login/web/login/LoginController.java
+++ spring/spring_mvc_2/login/src/main/java/hello/login/web/login/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -71,7 +72,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+//    @PostMapping("/login")
     public String loginV3(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response, HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
@@ -90,6 +91,30 @@ public class LoginController {
         session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
 
         return "redirect:/";
+    }
+
+    // 권한 없이 접근 시 로그인 페이지로 갔다가 원래 접근하던 페이지로 돌아가는 코드 추가
+    @PostMapping("/login")
+    public String loginV4(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult,
+                          @RequestParam(defaultValue = "/") String redirectURL,
+                          HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+        Member loginMember = loginService.login(loginForm.getLoginId(), loginForm.getPassword());
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        // 로그인 성공 처리
+        // 세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
+        // getSession(true) : 세션이 있으면 기존 세션 반환, 세션이 없으면 생성해서 반환, false 일 경우 세션이 있으면 기존 세션 동일하게 반환하나, 없으면 null 반환
+        HttpSession session = request.getSession();
+        // 세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        return "redirect:" + redirectURL;
     }
 
 //    @PostMapping("/logout")
