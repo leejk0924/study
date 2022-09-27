4e58e0d14a7a5fb2baeec56adb3c06000a450c78
Jung-Kwon Lee <akdirdl@gmail.com>
2022-09-27 10:38:56 +0900

    lecture:스프링 MVC 2편 (예외처리와 오류 페이지)
    예외 처리와 오류 페이지 (서블릿 예외 처리 - 인터셉터)

--- spring/spring_mvc_2/exception/exception/src/main/java/hello/exception/WebConfig.java
+++ spring/spring_mvc_2/exception/exception/src/main/java/hello/exception/WebConfig.java
@@ -1,9 +1,13 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -12,15 +16,23 @@ import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");   // 오류 페이지 경로 (중복 호출 제거)
+    }
+
     @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
         filterRegistrationBean.setOrder(1);
         filterRegistrationBean.addUrlPatterns("/*");
-        // DispatcherType 이 REQUEST 와 ERROR 일 경우 호출되게 설정
-        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+        // DispatcherType 이 REQUEST 와 ERROR 일 경우 호출되게 설정  (중복 호출 제거)
+//        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST);
         return filterRegistrationBean;
-
     }
 }
