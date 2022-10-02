f5406621150b1e853aa09879c451ee883d55b5b6
Jung-Kwon Lee <akdirdl@gmail.com>
2022-10-03 03:10:31 +0900

    lecture:스프링 MVC 2편 (스프링 타입 컨버터)
    스프링 타입 컨버터 (타입 컨버터 - Converter)

---
+++ spring/spring_mvc_2/typeconverter/src/test/java/com/hello/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,41 @@
+package com.hello.typeconverter.converter;
+
+
+import com.hello.typeconverter.type.IpPort;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConverterTest {
+
+    @Test
+    void StringToInteger() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    void IntegerToStringConverter() {
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(10);
+        assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    void StringToIpPort() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(source);
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void ipPortToString() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        IpPort result = converter.convert(source);
+        // @EqualsAndHashCode 를 IpPort 에 넣었기 때문에 참조값이 달라도 비교가 된다.
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+}
