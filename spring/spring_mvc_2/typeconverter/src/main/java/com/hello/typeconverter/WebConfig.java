52a4c7f4733fdd6ba74194002dc0987d4998c4a6
Jung-Kwon Lee <akdirdl@gmail.com>
2022-10-03 16:29:23 +0900

    lecture:스프링 MVC 2편 (스프링 타입 컨버터)
    스프링 타입 컨버터 (포맷터 적용하기)

--- spring/spring_mvc_2/typeconverter/src/main/java/com/hello/typeconverter/WebConfig.java
+++ spring/spring_mvc_2/typeconverter/src/main/java/com/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import com.hello.typeconverter.converter.IntegerToStringConverter;
 import com.hello.typeconverter.converter.IpPortToStringConverter;
 import com.hello.typeconverter.converter.StringToIntegerConverter;
 import com.hello.typeconverter.converter.StringToIpPortConverter;
+import com.hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,9 +13,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        // 주선 처리 : 우선순위 때문에  [Converter 가 Formatter 보다 우선순위가 더 높다]
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        // 추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
