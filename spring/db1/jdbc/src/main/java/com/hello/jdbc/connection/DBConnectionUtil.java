2a3c468bd748f7f65407a0552f962d11148e2451
Jung-Kwon Lee <akdirdl@gmail.com>
2022-10-09 02:26:27 +0900

    lecture: 스프링 DB 1편 (커넥션풀과 데이터소스 이해)
    DataSource 예제1 - DriverManager

--- spring/db1/jdbc/src/main/java/com/hello/jdbc/connection/DBConnectionUtil.java
+++ spring/db1/jdbc/src/main/java/com/hello/jdbc/connection/DBConnectionUtil.java
@@ -6,11 +6,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import static com.hello.jdbc.connection.ConnectionCons.*;
+import static com.hello.jdbc.connection.ConnectionConst.*;
 
 @Slf4j
 public class DBConnectionUtil {
-    // JDBC 표준 인터페이스가 제공해주는 ConnectionCons (java.sql.ConnectionCons)
+    // JDBC 표준 인터페이스가 제공해주는 ConnectionConst (java.sql.ConnectionConst)
     public static Connection getConnection() {
         try {
             // DriverManager 가 라이브러리에 있는 Driver 를 찾아서 DB 에 접근
