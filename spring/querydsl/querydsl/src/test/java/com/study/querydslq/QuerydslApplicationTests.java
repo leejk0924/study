7af506f4cd139143c4055407a8dcd652338a069b
Jung-Kwon Lee <akdirdl@gmail.com>
2022-10-11 11:30:48 +0900

    lecture: 실전! Querydsl (프로젝트 환경설정)
    Querydsl 설정과 검증

--- spring/querydsl/querydsl/src/test/java/com/study/querydslq/QuerydslApplicationTests.java
+++ spring/querydsl/querydsl/src/test/java/com/study/querydslq/QuerydslApplicationTests.java
@@ -1,13 +1,41 @@
 package com.study.querydslq;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.querydslq.entity.Hello;
+import com.study.querydslq.entity.QHello;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+    @PersistenceContext // 다른 프레임워크로 사용 시 해당 어노테이션 사용
+//    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
-    }
+        Hello hello = new Hello();
+        em.persist(hello);
 
+        // querydsl 사용하려면 JPAQueryFactory 사용을 권장함
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
 }
