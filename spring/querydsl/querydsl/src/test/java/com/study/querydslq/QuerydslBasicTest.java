1187bd7e0f0b61df0e56d0ca92566a3751ee0953
Jung-Kwon Lee <akdirdl@gmail.com>
2022-10-12 01:31:06 +0900

    lecture: 실전! Querydsl (기본 문법)
    상수, 문자 더하기

--- spring/querydsl/querydsl/src/test/java/com/study/querydslq/QuerydslBasicTest.java
+++ spring/querydsl/querydsl/src/test/java/com/study/querydslq/QuerydslBasicTest.java
@@ -3,6 +3,7 @@ package com.study.querydslq;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.study.querydslq.entity.Member;
@@ -427,5 +428,28 @@ public class QuerydslBasicTest {
         for (String s : result) {
             System.out.println("s = " + s);
         }
+    }
+
+    @Test
+    public void constant() {
+        List<Tuple> result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    public void concat() {
+        // {username}_{age}
+        List<String> result = queryFactory
+                // ENUM 처리할 떄 stringValue() 많이 사용한다.
+                .select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetch();
+        System.out.println("result = " + result);
     }
 }
