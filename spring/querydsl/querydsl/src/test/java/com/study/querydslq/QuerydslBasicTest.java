eb941bcde2703bf69243a157ed41d3d67d49c590
Jung-Kwon Lee <akdirdl@gmail.com>
2022-10-13 00:54:37 +0900

    lecture: 실전! Querydsl (중급 문법)
    동적 쿼리 - BooleanBuilder 사용

--- spring/querydsl/querydsl/src/test/java/com/study/querydslq/QuerydslBasicTest.java
+++ spring/querydsl/querydsl/src/test/java/com/study/querydslq/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package com.study.querydslq;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
@@ -573,5 +574,30 @@ public class QuerydslBasicTest {
         for (MemberDto memberDto : result) {
             System.out.println("memberDto = " + memberDto);
         }
+    }
+
+    @Test
+    public void dynamicQuery_BooleanBuilder() {
+        String usernameParam = "member1";
+        Integer ageParam = 10;
+
+        List<Member> result = searchMember1(usernameParam, ageParam);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+        // new BooleanBuilder();  <- 초기 값을 넣어줄 수 있다.
+        BooleanBuilder builder = new BooleanBuilder();
+        if (usernameCond != null) {
+            builder.and(member.username.eq(usernameCond));
+        }
+        if (ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+
+        return queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
     }
 }
