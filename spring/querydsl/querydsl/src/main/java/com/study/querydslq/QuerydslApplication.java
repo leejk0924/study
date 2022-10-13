d3af5610be9597e8ae094f875f793cd7c6591e0c
Jung-Kwon Lee <akdirdl@gmail.com>
2022-10-13 17:48:06 +0900

    lecture: 실전! Querydsl (JPA와 Querydsl)
    순수 JPA 리포지토리와 Querydsl

--- spring/querydsl/querydsl/src/main/java/com/study/querydslq/QuerydslApplication.java
+++ spring/querydsl/querydsl/src/main/java/com/study/querydslq/QuerydslApplication.java
@@ -1,7 +1,11 @@
 package com.study.querydslq;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class QuerydslApplication {
@@ -10,4 +14,11 @@ public class QuerydslApplication {
         SpringApplication.run(QuerydslApplication.class, args);
     }
 
+//     //JPAQueryFactory bean 에 등록
+    // 동시성 문제가 발생하지 않을까? 라는 의문 -> JPAQueryFactory 의 동시성 문제는 EntityManager 에 의존하고 EntityManager는
+    // 동시성 문제 없이 트랜잭션 단위로 분리되어 동작하므로 
+//    @Bean
+//    JPAQueryFactory jpaQueryFactory(EntityManager em) {
+//        return new JPAQueryFactory(em);
+//    }
 }
