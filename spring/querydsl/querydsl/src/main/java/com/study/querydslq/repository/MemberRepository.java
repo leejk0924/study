1f5f29ebc164d724bf3e40c7e78e5616ba72fd4f
Jung-Kwon Lee <akdirdl@gmail.com>
2022-10-17 10:33:11 +0900

    lecture: 실전! Querydsl (스프링 데이터 JPA가 제공하는 Querydsl 기능)
    인터페이스 지원 - QuerydslPredicateExecutor

--- spring/querydsl/querydsl/src/main/java/com/study/querydslq/repository/MemberRepository.java
+++ spring/querydsl/querydsl/src/main/java/com/study/querydslq/repository/MemberRepository.java
@@ -2,10 +2,11 @@ package com.study.querydslq.repository;
 
 import com.study.querydslq.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor {
     // select m from Member m where m.username=?
     List<Member> findByUsername(String username);
 }
--- spring/querydsl/querydsl/src/test/java/com/study/querydslq/repository/MemberRepositoryTest.java
+++ spring/querydsl/querydsl/src/test/java/com/study/querydslq/repository/MemberRepositoryTest.java
@@ -3,6 +3,7 @@ package com.study.querydslq.repository;
 import com.study.querydslq.dto.MemberSearchCondition;
 import com.study.querydslq.dto.MemberTeamDto;
 import com.study.querydslq.entity.Member;
+import com.study.querydslq.entity.QMember;
 import com.study.querydslq.entity.Team;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -91,5 +92,30 @@ class MemberRepositoryTest {
         Page<MemberTeamDto> result = memberRepository.searchPageSimple(condition, pagerequest);
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
+    }
+
+    @Test
+    public void querydslPredicateExecutorTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1); 
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+        Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("member1")));
+        for (Member findMember : result) {
+            System.out.println("member5 = " + findMember);
+        }
     }
 }
