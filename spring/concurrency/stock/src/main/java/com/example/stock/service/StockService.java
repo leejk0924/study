991b1912bfb1bd3ef2bd3701799ddb9484c376b6
Jung-Kwon Lee <akdirdl@gmail.com>
2022-11-02 01:23:55 +0900

    lecture: 재고시스템으로 알아보는 동시성이슈 해결방법
    Synchronized 이용해보기 - Synchronized 이용해보기

--- spring/concurrency/stock/src/main/java/com/example/stock/service/StockService.java
+++ spring/concurrency/stock/src/main/java/com/example/stock/service/StockService.java
@@ -11,9 +11,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class StockService {
 
     final private StockRepository stockRepository;
-
+    // synchronized 를 해도 Transactional 로 인해 레이스 컨디션 문제가 해결되지 않는다.
+    // 왜냐하면 해당 메서드는 동기화처리 해도 Transactional 이 끝나기 전에 다른 쓰레드가 해당 메서드에 접근이 가능하여 정상적인 테스트가 진행되지 않는다.
+    // Transactional 을 지우면 해당 테스트는 정상적으로 작동한다.
     @Transactional
-    public void decrease(Long id, Long quantity) {
+    public synchronized void decrease(Long id, Long quantity) {
         // get stock
         Stock stock = stockRepository.findById(id).orElseThrow();
         // 재고 감소
