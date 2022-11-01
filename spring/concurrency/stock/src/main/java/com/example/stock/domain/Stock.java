f115229b6320c1dfdfcb237c4b04a3656496ca44
Jung-Kwon Lee <akdirdl@gmail.com>
2022-11-02 00:12:23 +0900

    lecture: 재고시스템으로 알아보는 동시성이슈 해결방법
    재고시스템 만들어보기 - 재고감소 로직작성

---
+++ spring/concurrency/stock/src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,35 @@
+package com.example.stock.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public void decrease(Long quantity) {
+        if (this.quantity - quantity < 0) {
+            throw new RuntimeException("0개 미만입니다.");
+        }
+        this.quantity = this.quantity - quantity;
+    }
+}
