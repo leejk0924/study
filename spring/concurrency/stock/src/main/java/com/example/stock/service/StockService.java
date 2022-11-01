1d5ce54145526052c4f8a3144f2e106d1f333183
Jung-Kwon Lee <akdirdl@gmail.com>
2022-11-02 02:53:57 +0900

    lecture: 재고시스템으로 알아보는 동시성이슈 해결방법
    Database 이용해보기 - Named Lock 활용해보기

--- spring/concurrency/stock/src/main/java/com/example/stock/service/StockService.java
+++ spring/concurrency/stock/src/main/java/com/example/stock/service/StockService.java
@@ -4,6 +4,7 @@ import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -14,7 +15,8 @@ public class StockService {
     // synchronized 를 해도 Transactional 로 인해 레이스 컨디션 문제가 해결되지 않는다.
     // 왜냐하면 해당 메서드는 동기화처리 해도 Transactional 이 끝나기 전에 다른 쓰레드가 해당 메서드에 접근이 가능하여 정상적인 테스트가 진행되지 않는다.
     // Transactional 을 지우면 해당 테스트는 정상적으로 작동한다.
-    @Transactional
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public synchronized void decrease(Long id, Long quantity) {
         // get stock
         Stock stock = stockRepository.findById(id).orElseThrow();
---
+++ spring/concurrency/stock/src/test/java/com/example/stock/facade/NamedLockStockFacadeTest.java
@@ -0,0 +1,68 @@
+package com.example.stock.facade;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class NamedLockStockFacadeTest {
+
+    @Autowired
+    private NamedLockStockFacade namedLockStockFacade;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void insert() {
+        Stock stock = new Stock(1L, 100L);
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+
+    @Test()
+    @DisplayName("동시에 100개의 요청")
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+
+        // 비동기로 실행하는 작업을 단순화 하여 사용할 수 있게 도와주는 java api
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        // 다른 쓰레드에서 실행중인 작업을 대기할 수 있도록 도와주는 클래스
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                        try {
+                            namedLockStockFacade.decrease(1L, 1L);
+                        } finally {
+                            latch.countDown();
+                        }
+                    }
+            );
+        }
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 -(1*199) = 0
+        assertEquals(0, stock.getQuantity());
+    }
+}
