fc543dc5d31360c75a77f39e4ec7b53d154f09b0
Jung-Kwon Lee <akdirdl@gmail.com>
2022-11-02 01:15:59 +0900

    lecture: 재고시스템으로 알아보는 동시성이슈 해결방법
    재고시스템 만들어보기 - 문제점

--- spring/concurrency/stock/src/test/java/com/example/stock/service/StockServiceTest.java
+++ spring/concurrency/stock/src/test/java/com/example/stock/service/StockServiceTest.java
@@ -9,6 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -39,5 +43,35 @@ class StockServiceTest {
         Assertions.assertThat(stock.getQuantity()).isEqualTo(99);
 //        assertEquals(99, stock.getQuantity());
 
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+
+        // 비동기로 실행하는 작업을 단순화 하여 사용할 수 있게 도와주는 java api
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        // 다른 쓰레드에서 실행중인 작업을 대기할 수 있도록 도와주는 클래스
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                        try {
+                            stockService.decrease(1L, 1L);
+
+                        } finally {
+                            latch.countDown();
+                        }
+                    }
+            );
+        }
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 -(1*199) = 0
+
+        assertEquals(0, stock.getQuantity());
     }
 }
