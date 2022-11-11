c3688c14008e6ead30e22ec026514cbc38ccf2f6
Jung-Kwon Lee <akdirdl@gmail.com>
2022-11-12 01:19:52 +0900

    lecture: hodol dev
    프로젝트 생성

---
+++ spring/hodol/hodol/src/test/java/com/example/hodol/HodolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hodol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HodolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
