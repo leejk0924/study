461507768855a1dc89059d79e883619f0af5e220
Jung-Kwon Lee <akdirdl@gmail.com>
2022-11-22 16:28:18 +0900

    lecture: hodol dev
    문서화 : Spring REST Docs1 - 기본설정

---
+++ spring/hodol/hodol/src/test/java/com/example/hodol/controller/BoardControllerDocTest.java
@@ -0,0 +1,57 @@
+package com.example.hodol.controller;
+
+import com.example.hodol.domain.Board;
+import com.example.hodol.repository.BoardRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+@SpringBootTest
+@ExtendWith(RestDocumentationExtension.class)
+public class BoardControllerDocTest {
+    private MockMvc mockMvc;
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
+
+    @Test
+    @DisplayName("글 단건 조회 테스트")
+    void test1() throws Exception {
+        Board board = Board
+                .builder()
+                .title("제목")
+                .content("내용")
+                .build();
+        boardRepository.save(board);
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/posts/{postId}", 1L)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcRestDocumentation.document("index"));
+    }
+}
+
+/**
+ * 프로젝트의 RestDocumentationExtension 빌드 도구를 기반으로 하는 출력 디렉터리로 자동 구성된다.
+ */
